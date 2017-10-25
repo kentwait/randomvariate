3c39668f102b9ad1c17432b1c47a992970a720f7
kent <kentkawashima@gmail.com>
2017-10-25 16:28:32 +0900

    Adds docs

--- binomial.go
+++ binomial.go
@@ -1,5 +1,11 @@
 package randomvariate
 
-func Binomial(n int, p float64) []int {
-	return Multinomial(n, []float64{p, 1.0 - p})
+// Binomial draws n samples from a binomial probability distribution given
+// by the probability p.
+// Another way to imagine this is that the function counts the number of
+// sucessful draws. If n = 1, the function draws once and the result can
+// either be 1 or 0. The probability of successfully drawing is given by
+// the slice p.
+func Binomial(n int, p float64) int {
+	return Multinomial(n, []float64{p, 1.0 - p})[0]
 }
